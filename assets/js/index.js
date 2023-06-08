5589e82d1e6f198ecc9ec8ff6fa87a6a0e05332f
Ariel Wardana <112913828+ArielGwd@users.noreply.github.com>
2023-06-09 00:58:19 +0700

    update
    update gambar hilang, title, icon, klick, animasi

--- assets/js/index.js
+++ assets/js/index.js
@@ -30,8 +30,10 @@ let i;
 
 for (i = 0; i < coll.length; i++) {
   coll[i].addEventListener("click", function() {
+    
     this.classList.toggle("active-bre");
     let content = this.nextElementSibling;
+
     if (content.style.display === "block") {
       content.style.display = "none";
     } else {
